ec1a92f998b81365ad3e31ae69272823209b96d4
Jason Stajich <jasonstajich.phd@gmail.com>
2018-05-06 13:03:50 -0700

    Some cluster searching

---
+++ C_hunter/runs.sh
@@ -0,0 +1,2 @@
+/usr/local/pkg/C_Hunter-1.0/bin/C_Hunter Scheme/scheme.GO.data Data_set/Coprinus_cinereus/map_list Data_set/Coprinus_cinereus/data_list 3 0 0 0.001 50 t CC_nomax >& CC_nomax.out
+/usr/local/pkg/C_Hunter-1.0/bin/C_Hunter Scheme/scheme.GO.data Data_set/Coprinus_cinereus/map_list Data_set/Coprinus_cinereus/data_list 3 300 0 0.001 10 t CC_max300 >& CC_max300.out
---
+++ C_hunter/runs_lbic.sh
@@ -0,0 +1,2 @@
+/usr/local/pkg/C_Hunter-1.0/bin/C_Hunter Scheme/scheme.GO.data Data_set/Laccaria_bicolor/map_list Data_set/Laccaria_bicolor/data_list 3 300 0 0.001 10 t LB_max300 >& LB_max300.out
+/usr/local/pkg/C_Hunter-1.0/bin/C_Hunter Scheme/scheme.GO.data Data_set/Laccaria_bicolor/map_list Data_set/Laccaria_bicolor/data_list 3 0 0 0.001 50 t LB_nomax >& LB_nomax.out
